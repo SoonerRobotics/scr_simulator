d3fbf2a7a3557bd0181f13259818806f58352e27
Noah Zemlin <noah.zemlin@gmail.com>
2019-11-20 18:19:50 -0600

    Stephanie is a god

--- Assets/SimpleCarController.cs
+++ Assets/SimpleCarController.cs
@@ -6,9 +6,10 @@ public class SimpleCarController : MonoBehaviour
 {
 
     public float speedMod = 1f;
-    public float turnMod = 100f;
+    public float turnMod = 1f;
     public float wheelRadius = 0.127f;
     public float L = 0.6096f;
+    public float Q = 0.3456f;
 
     float vl = 0;
     float vr = 0;
@@ -23,17 +24,14 @@ public class SimpleCarController : MonoBehaviour
     public void FixedUpdate()
     {
 
-        float x = transform.position.z;
-        float y = -transform.position.x;
-        float psi = -transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
-
+        float psi = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
 
         float left = Mathf.Pow(Input.GetAxis("Vertical"),3);
-        float right = Mathf.Pow(Input.GetAxis("Vertical2"),3);
+        float right = -Mathf.Pow(Input.GetAxis("Vertical2"),3);
 
         Debug.Log("vl: " + vl + ", vr: " + vr);
 
-        float psi_dot = (vl + vr) / L;
+        float psi_dot = (vl - vr) / L;
 
         vl = (float)System.Math.Tanh(left + vl);
         vr = (float)System.Math.Tanh(right + vr);
@@ -41,24 +39,15 @@ public class SimpleCarController : MonoBehaviour
         if (vr - vl != 0)
         {
 
-            float R = L * (vl + vr) / (2 * (vr - vl));
-            float ICC_x = x - R * Mathf.Sin(psi);
-            float ICC_y = y + R * Mathf.Cos(psi);
-
             Debug.Log("psidot: " + psi_dot);
 
-            if (R < 100)
-            {
-                float new_x = ((x - ICC_x) * Mathf.Cos(psi_dot * Time.deltaTime) - (y - ICC_y) * Mathf.Sin(psi_dot * Time.deltaTime) + ICC_x);
-                float new_y = ((x - ICC_x) * Mathf.Sin(psi_dot * Time.deltaTime) + (y - ICC_y) * Mathf.Cos(psi_dot * Time.deltaTime) + ICC_y);
-                float dx = new_x - x;
-                float dy = new_y - y;
+            float dot_x = (vr + (vl - vr) / 4.0f) * Mathf.Sin(psi);
+            float dot_y = (vr + (vl - vr) / 4.0f) * Mathf.Cos(psi);
 
-                transform.Translate(new Vector3(-dy, 0, dx) * speedMod, Space.World);
-                transform.Rotate(new Vector3(0, -psi_dot * Mathf.Rad2Deg, 0) * Time.deltaTime * turnMod);
+            transform.Translate(new Vector3(dot_x, 0, dot_y) * Time.deltaTime * speedMod, Space.World);
 
-                return;
-            }
+            transform.Rotate(new Vector3(0, psi_dot * Mathf.Rad2Deg, 0) * Time.deltaTime * turnMod, Space.World);
+            return;
         }
 
         float dx2 = wheelRadius / 2.0f * (vl + vr) * Mathf.Cos(psi);
@@ -66,7 +55,7 @@ public class SimpleCarController : MonoBehaviour
 
         Debug.Log("dx: " + dx2 + ", dy: " + dy2);
 
-        transform.Translate(new Vector3(-dy2, 0, dx2) * speedMod, Space.World);
+        // transform.Translate(new Vector3(-dy2, 0, dx2) * speedMod, Space.World);
         // transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime * turnMod);
     }
 }
