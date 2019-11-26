ee6f51c0bd0e17c8283e09a8f285dd70d0b4422c
Noah Zemlin <noah.zemlin@gmail.com>
2019-11-25 21:02:00 -0600

    Add camera scrolling

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    void OnGUI()
+    {
+        this.transform.Translate(Input.mouseScrollDelta.y * this.transform.forward * speed);
+    }
+}
