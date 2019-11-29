81a43a386d0a1df982477b0ef8b5e64a53ad1928
Noah Zemlin <noah.zemlin@gmail.com>
2019-11-29 14:05:17 -0600

    Pause menu and compass

---
+++ Assets/CompassScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CompassScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI textMeshProUgui;
+    public GameObject body;
+
+    // Via https://gist.github.com/adrianstevens/8163205
+    public static string DegreesToCardinal(double degrees)
+    {
+        string[] caridnals = { "N", "NE", "E", "SE", "S", "SW", "W", "NW", "N" };
+        return caridnals[ (int)Math.Round(((double)degrees % 360) / 45) ];
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float heading = body.transform.rotation.eulerAngles.y;
+        textMeshProUgui.text = $"{heading:0}° {DegreesToCardinal(heading)}";
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject pauseUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        if (!isPaused)
+        {
+            isPaused = true;
+            pauseUI.SetActive(true);
+        }
+        else
+        {
+            isPaused = false;
+            pauseUI.SetActive(false);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+}
