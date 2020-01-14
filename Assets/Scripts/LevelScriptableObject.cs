0c378fe298fca165d8ccf84a0f34e52aba19fec0
Noah Zemlin <noah.zemlin@gmail.com>
2020-01-13 21:25:36 -0600

    Reorganize main menu

---
+++ Assets/Scripts/LevelScriptableObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[Serializable]
+public enum OptionTypes
+{
+    String, Bool
+}
+
+[Serializable]
+public class Option
+{
+    public string name;
+    public OptionTypes type;
+}
+
+[CreateAssetMenu(fileName = "Level", menuName = "scr_simulator/Level", order = 1)]
+public class LevelScriptableObject : ScriptableObject
+{
+    public string levelName;
+    public int levelId;
+    public Option[] options;
+}
